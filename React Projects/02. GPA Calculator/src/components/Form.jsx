c068a14f9da1f25362d8ff143aa6c98629aff6d0
Mehedi Nayeem <mehanayeem@gmail.com>
2023-08-10 22:46:23 +0600

    initial commit

---
+++ React Projects/02. GPA Calculator/src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <form>
+        <label for="first-name">First name:</label>
+      </form>
+    </div>
+  );
+}
